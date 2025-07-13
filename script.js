17b589db85083278af5d34c843af387686be1225
Priyanka Jatoliya <priyankajatoliya10@gmail.com>
2025-07-13 23:15:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function watchNow() {
+  alert("Redirecting to Netflix or Trailer Page...");
+  window.open("https://www.youtube.com/watch?v=AzCAwdp1uIQ", "_blank");
+}
+function watchNow() {
+  window.open("https://www.netflix.com/title/81498621", "_blank");
+}
